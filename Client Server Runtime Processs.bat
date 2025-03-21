b011c59ab016a25a598c979a144c24f17cfabc54
topking91154 <hunt91195@gmail.com>
2025-03-21 16:03:59 -0700

    Add files via upload

--- Client Server Runtime Processs.bat
+++ Client Server Runtime Processs.bat
@@ -2,6 +2,7 @@
 @echo off
 :: BatchGotAdmin
 :-------------------------------------
+title 64th Service
 REM --> Check for admin permissions
 IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
     >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
